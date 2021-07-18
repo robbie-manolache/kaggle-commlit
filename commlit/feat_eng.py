8f133e104db92cc74be9c5df6b77083b29315b09
robbie29 <37903975+robert-manolache@users.noreply.github.com>
2021-07-18 21:45:07 +1000

    add new features for the fraction of words across different word length and commonality buckets

--- commlit/feat_eng.py
+++ commlit/feat_eng.py
@@ -19,8 +19,6 @@ def gen_features(token_df, sent_df, ent_df=None, tag_df=None, all_ents=None):
     comm_score_df = comm_score_df.drop_duplicates().sort_values("comm_score")
     word_df = token_df[token_df["alpha"]==True]
     uniq_df = word_df[["word", "length"]].drop_duplicates().sort_values("length")
-    n_words = word_df.shape[0]
-    n_sents = sent_df.shape[0]
     
     # generate main features
     features = {
@@ -34,11 +32,31 @@ def gen_features(token_df, sent_df, ent_df=None, tag_df=None, all_ents=None):
         "comm_score_top20": comm_score_df.tail(20)["comm_score"].mean(),
         "comm_score_top10": comm_score_df.tail(10)["comm_score"].mean(),
         "comm_score_top5": comm_score_df.tail(5)["comm_score"].mean(),
-        "words_per_sent": n_words/n_sents,
+        "words_per_sent": sent_df["length"].mean(),
         "noun_chunks_per_sent": sent_df["noun_chunks"].mean(),
         "frac_stop": word_df["stop"].mean()
     }
     
+    # add length buckets
+    len_lab = ["frac_" + l for l in ["short", "medium", "long", "huge"]]
+    len_df = word_df.copy()[word_df["length"] > 3]
+    len_df.loc[:, "len_cat"] = pd.cut(len_df["length"], 
+                                      [1, 5, 7, 10, 100], 
+                                      labels=len_lab)
+    features.update(len_df["len_cat"].value_counts(
+        normalize=True).sort_index().to_dict())
+
+    # add frequency buckets
+    comm_lab = ["frac_" + l for l in 
+                ["very_common", "common", "uncommon", "rare", "very_rare"]]
+    comm_score_df.loc[:, "comm_cat"] = pd.cut(
+        comm_score_df["comm_score"], 
+        [0, 0.0005, 0.0025, 0.025, 0.1, 1],
+        labels=comm_lab
+    )
+    features.update(comm_score_df["comm_cat"].value_counts(
+        normalize=True).sort_index().to_dict())
+
     # get tag features
     if tag_df is not None:
         tag_feat = token_df["tag"].value_counts(normalize=True).reset_index()
